257b2e7722859538bfd1a0ed6dd82e8144818fe5
Naohiro Takahashi <chokudai@gmail.com>
2023-12-22 12:56:30 +0900

    プロジェクト ファイルを追加します。

---
+++ Solver/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+class Field
+{
+    int N, M, K, T;
+
+    double[] GuessX;
+
+    public Field(int N, int M, int K, int T)
+    {
+        this.N = N;
+        this.M = M;
+        this.K = K;
+        this.T = T;
+
+    }
+
+}
+
+class State
+{
+    static public int Field;
+
+
+}
+
+class Solver
+{
+    //上げて実行するときは必ずCommitする！
+    int SolverVersion = 1;
+
+    public static int Main()
+    {
+
+
+    }
+
+
+}
+
